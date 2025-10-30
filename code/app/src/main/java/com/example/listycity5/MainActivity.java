4700c7247d9c894f43a5fcaecc57724b0c8abcb0
friedchickenblob <jck2@ualberta.ca>
2025-10-29 20:12:07 -0600

    add code folder

---
+++ code/app/src/main/java/com/example/listycity5/MainActivity.java
@@ -0,0 +1,139 @@
+package com.example.listycity5;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity implements CityDialogFragment.CityDialogListener {
+
+    private Button addCityButton;
+    private ListView cityListView;
+
+    private ArrayList<City> cityArrayList;
+    private ArrayAdapter<City> cityArrayAdapter;
+    private FirebaseDatabase db;
+    private DatabaseReference citiesRef;
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        db = FirebaseDatabase.getInstance("https://listycity-friedchicken-default-rtdb.firebaseio.com/");
+        citiesRef = db.getReference("bruhmoment");
+
+        // Set views
+        addCityButton = findViewById(R.id.buttonAddCity);
+        cityListView = findViewById(R.id.listviewCities);
+
+        // create city array
+        cityArrayList = new ArrayList<>();
+        cityArrayAdapter = new CityArrayAdapter(this, cityArrayList);
+        cityListView.setAdapter(cityArrayAdapter);
+
+//        addDummyData();
+
+        // set listeners
+        addCityButton.setOnClickListener(view -> {
+            CityDialogFragment cityDialogFragment = new CityDialogFragment();
+            cityDialogFragment.show(getSupportFragmentManager(),"Add City");
+        });
+
+        cityListView.setOnItemClickListener((adapterView, view, i, l) -> {
+            City city = cityArrayAdapter.getItem(i);
+            CityDialogFragment cityDialogFragment = CityDialogFragment.newInstance(city);
+            cityDialogFragment.show(getSupportFragmentManager(),"City Details");
+        });
+
+        citiesRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                cityArrayList.clear();
+                for (DataSnapshot citySnapshot : snapshot.getChildren()) {
+                    String name = citySnapshot.getKey();
+                    String province = citySnapshot.child("Province").getValue(String.class);
+                    cityArrayList.add(new City(name, province));
+                    Log.d("Firebase", name + " -> " + province);
+                }
+                cityArrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("Firebase", "Read failed", error.toException());
+
+            }
+        });
+
+    }
+
+    @Override
+    public void updateCity(City city, String title, String year) {
+        citiesRef.child(city.getName()).removeValue()
+                .addOnSuccessListener(a -> Log.d("FirestoreTest", "Success remove"))
+                .addOnFailureListener(e -> Log.e("FirestoreTest", "Failed remove", e));;;
+        city.setName(title);
+        city.setProvince(year);
+        cityArrayAdapter.notifyDataSetChanged();
+
+        HashMap<String, String> data = new HashMap<>();
+        data.put("Province", city.getProvince());
+
+        citiesRef.child(city.getName()).setValue(data)
+                .addOnSuccessListener(a -> Log.d("FirestoreTest", "Success"))
+                .addOnFailureListener(e -> Log.e("FirestoreTest", "Failed", e));;
+        // Updating the database using delete + addition
+    }
+
+    @Override
+    public void addCity(City city){
+        cityArrayList.add(city);
+        cityArrayAdapter.notifyDataSetChanged();
+        HashMap<String, String> data = new HashMap<>();
+        data.put("Province", city.getProvince());
+        Log.e("Firestore", "what");
+        citiesRef.child(city.getName()).setValue(data)
+                .addOnSuccessListener(a -> Log.d("FirestoreTest", "Success"))
+                .addOnFailureListener(e -> Log.e("FirestoreTest", "Failed", e));;
+    }
+
+    @Override
+    public void deleteCity(City city) {
+        cityArrayList.remove(city);
+        cityArrayAdapter.notifyDataSetChanged();
+    }
+
+//    public void addDummyData(){
+//        City m1 = new City("Edmonton", "AB");
+//        City m2 = new City("Vancouver", "BC");
+//        cityArrayList.add(m1);
+//        cityArrayList.add(m2);
+//        cityArrayAdapter.notifyDataSetChanged();
+//    }
+}
