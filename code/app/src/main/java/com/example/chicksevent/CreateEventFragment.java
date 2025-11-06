135d2ab8f40310ca38b8d080831bdffe09b83302
Jin Kasai <skasai@ualberta.ca>
2025-11-05 18:04:21 -0700

    connected create event ui to the logic

--- code/app/src/main/java/com/example/chicksevent/CreateEventFragment.java
+++ code/app/src/main/java/com/example/chicksevent/CreateEventFragment.java
@@ -5,4 +5,8 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 public class CreateEventFragment extends Fragment {
+    public CreateEventFragment() {
+        super(R.layout.fragment_create_event); // <- inflates your XML
+    }
 }
+
