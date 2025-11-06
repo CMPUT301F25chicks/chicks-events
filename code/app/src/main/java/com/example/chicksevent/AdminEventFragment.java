88d1eb8d10f4b0946abb82ce40b689e56afa4a18
DamHanhNguyenMong <damhanhn@ualberta.ca>
2025-11-06 00:35:23 -0700

    WIP: save current work before merging admin-jordan

--- code/app/src/main/java/com/example/chicksevent/AdminEventFragment.java
+++
@@ -1,7 +0,0 @@
-package com.example.chicksevent;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
-
-
-public class AdminEventFragment extends Fragment{
-}
--- code/app/src/main/java/com/example/chicksevent/AdminProfileFragment.java
+++
@@ -1,6 +0,0 @@
-package com.example.chicksevent;
-
-import androidx.fragment.app.Fragment;
-
-public class AdminProfileFragment extends Fragment {
-}
